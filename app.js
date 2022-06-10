a4ce0b1b5e895ce71146921666fda5de8086adea
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-10 20:13:28 +0200

    Email Subscription added

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require("https");
 
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', function(req, res){
-    // res.send("This is running");
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/0f6d6ea1e4";
+
+    const options = {
+        method: "POST",
+        auth:"swarna: 26ea06437574743d483c3304d01af5be-us17"
+    }
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
 })
 
-app.listen(4000, function(){
+app.listen(process.env.PORT || 4000, function () {
     console.log("This is running on port 4000");
-})
+});
+
+//API Key
+// 26ea06437574743d483c3304d01af5be-us17
+
+//List ID/ Audience ID
+//0f6d6ea1e4
+
